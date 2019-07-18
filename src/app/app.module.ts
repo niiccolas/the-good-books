f829a93187321d9440050a61b6c0cbcf1ee0d182
jessiebarre <49344338+jessiebarre@users.noreply.github.com>
2019-07-18 12:09:06 +0200

    Add main app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchComponent } from './components/search/search.component';
+import { BooksComponent } from './components/books/books.component';
+import { DetailsComponent } from './components/details/details.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    BooksComponent,
+    DetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent implements OnInit {
+  @Output() componentSelected = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelectedComponent(component: string) {
+    this.componentSelected.emit(component);
+  }
+
+}
