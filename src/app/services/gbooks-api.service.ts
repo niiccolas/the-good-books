c055998b1c093b3ee3c0d9fab7ffb25aca699660
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-07-18 19:57:26 +0200

    Add service connecting to Google Books Api

--- src/app/services/gbooks-api.service.ts
+++ src/app/services/gbooks-api.service.ts
@@ -5,16 +5,11 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   providedIn: 'root'
 })
 export class GbooksApiService {
+  constructor(private http: HttpClient) { }
 
-  books = null;
-
-  constructor(private http: HttpClient) {
-    const bookParams = new HttpParams().set('q', 'Guillaume+Musso');
-
-    http.get('https://www.googleapis.com/books/v1/volumes', { params: bookParams }).subscribe((apiData) => {
-      console.log('-------GBOOKS API REQUEST---------');
-      console.log(apiData.items);
-      this.books = apiData.items;
-    });
+  queryApi(query: string, startIndex: number = 0, maxResults: number = 10) {
+    return (this.http.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&startIndex=${startIndex}`
+    ));
   }
 }
