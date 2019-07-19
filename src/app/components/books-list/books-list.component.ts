a58319b3f0205e0b9f8cfa852c9e7e90d7044571
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-07-19 18:42:38 +0200

    Add data sharing from BooksList to BooksItem

--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-books-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books-list.component.sass']
 })
 export class BooksListComponent implements OnInit {
+
+  @Input() userQueryResults: any;
 
   constructor() { }
 
