12fce8156c7e6c61b0f8a78b60c905a67055616c
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-07-19 15:17:58 +0200

    Add data sharing from parent to BooksItem component

--- src/app/components/books-item/books-item.component.ts
+++ src/app/components/books-item/books-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-books-item',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books-item.component.sass']
 })
 export class BooksItemComponent implements OnInit {
+
+  @Input() book: any;
 
   constructor() { }
 
