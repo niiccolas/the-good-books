919c86e8a27c344ca33e7845e08c4f0035875b21
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-07-19 15:30:47 +0200

    Add BooksList component skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SearchComponent } from './components/search/search.component';
-import { BooksComponent } from './components/books/books.component';
 import { DetailsComponent } from './components/details/details.component';
 import { CartComponent } from './components/cart/cart.component';
+import { BooksComponent } from './components/books/books.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
+import { BooksItemComponent } from './components/books-item/books-item.component';
 
 // SERVICES
 import { HttpClientModule } from '@angular/common/http';
-import { BooksItemComponent } from './components/books-item/books-item.component';
 
 @NgModule({
   declarations: [
@@ -23,9 +24,10 @@ import { BooksItemComponent } from './components/books-item/books-item.component
     HeaderComponent,
     FooterComponent,
     SearchComponent,
-    BooksComponent,
     DetailsComponent,
     CartComponent,
+    BooksComponent,
+    BooksListComponent,
     BooksItemComponent
   ],
   imports: [
