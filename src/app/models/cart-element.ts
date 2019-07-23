94eb642a685eea780349ed4ddd64e4a4d9984f01
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-07-23 18:21:00 +0200

    Apply billing rule to priceless elements: set to 10€

--- src/app/models/cart-element.ts
+++ src/app/models/cart-element.ts
@@ -2,9 +2,19 @@ export class CartElement {
   constructor(apiObject: any) {
     this.title        = apiObject.volumeInfo.title,
     this.author       = apiObject.volumeInfo.authors,
-    this.price        = apiObject.saleInfo.retailPrice.amount,
-    this.currencyCode = apiObject.saleInfo.retailPrice.currencyCode,
+    this.price        = this.validatePrice(apiObject.saleInfo),
+    this.currencyCode = 'EUR',
     this.thumbnail    = apiObject.volumeInfo.imageLinks.thumbnail;
   }
-}
 
+  validatePrice(saleInfo) {
+    switch (saleInfo.saleability) {
+      case 'FREE':
+        return 0;
+      case 'NOT_FOR_SALE':
+        return 10;
+      default:
+        return saleInfo.retailPrice.amount;
+    }
+  }
+}
