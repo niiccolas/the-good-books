a0f4713565723a184b040b7d0e58f9dbffd9a89b
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-07-24 17:56:41 +0200

    Handle discount on multiple books

--- src/app/components/cart-total/cart-total.component.ts
+++ src/app/components/cart-total/cart-total.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { CartElement } from 'src/app/models/cart-element';
 
 @Component({
   selector: 'app-cart-total',
@@ -17,19 +18,20 @@ export class CartTotalComponent implements OnInit {
     let total = 0;
 
     this.cartService.cartElements.forEach(book => {
-      total += this.applyDiscount(book.details.price, 5);
-    });
+        total += this.applyDiscount(book) * book.units;
+      }
+    );
 
-    return total;
+    return total.toFixed(2);
   }
 
-  applyDiscount(price: number, percentage: number) {
+  applyDiscount(book: CartElement) {
     // Round original price to two decimal places
-    price = +price.toFixed(2);
-
+    const price           = +book.details.price.toFixed(2);
+    const percentage      = (book.units >= 10) ? 10 : 5;
     const discountedPrice = price - (price * (percentage / 100));
 
     // Round discounted price to two decimal places
-    return +discountedPrice.toFixed(2);
+    return Number(discountedPrice.toFixed(2));
   }
 }
