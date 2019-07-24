8dd5c32b81ff0f7258dfe4ec3290f01e012658f0
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-07-24 13:02:16 +0200

    Add logic to compute total for the cart

--- src/app/components/cart-total/cart-total.component.ts
+++ src/app/components/cart-total/cart-total.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-total',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-total.component.sass']
 })
 export class CartTotalComponent implements OnInit {
+  grandTotal = this.computeTotal();
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  computeTotal() {
+    let total = 0;
+
+    this.cartService.cartElements.forEach(book => {
+      total += this.applyDiscount(book.price, 5);
+    });
+
+    return total;
   }
 
+  applyDiscount(price: number, percentage: number) {
+    // Round original price to two decimal places
+    price = +price.toFixed(2);
+
+    const discountedPrice = price - (price * (percentage / 100));
+
+    // Round discounted price to two decimal places
+    return +discountedPrice.toFixed(2);
+  }
 }
